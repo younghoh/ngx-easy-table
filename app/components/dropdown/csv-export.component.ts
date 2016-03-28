180b764026b9d5084b15a56dbcd38fd197d6a983
ssuperczynski <s.superczynski@gmail.com>
2016-03-28 19:07:56 +0200

    csv export

---
+++ app/components/dropdown/csv-export.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "angular2/core";
+@Component({
+    selector: 'csv-export',
+    templateUrl: 'app///components/dropdown/csv-export.html'
+})
+
+export class CsvExport {
+
+}
