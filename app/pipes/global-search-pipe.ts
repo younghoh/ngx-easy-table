dbb12dbf94220fb308f87786ddc065bc1cb0a9d0
sebastian <sebastian@listmsz-espeo.local>
2016-04-03 00:10:42 +0200

    code-style

--- app/pipes/global-search-pipe.ts
+++ app/pipes/global-search-pipe.ts
@@ -1,45 +1,45 @@
 import {Pipe} from "angular2/core";
 import {ResourceService} from "../services/resource-service";
 @Pipe({
-    name: 'global'
+  name: 'global'
 })
 
 export class GlobalSearchPipe {
-    constructor(public resource:ResourceService) {
-    }
+  constructor(public resource:ResourceService) {
+  }
 
-    transform(dataArr, [filter]) {
-        if (typeof dataArr === "undefined") {
+  transform(dataArr, [filter]) {
+    if (typeof dataArr === "undefined") {
+      return;
+    }
+    if (typeof filter === 'undefined' || Object.keys(filter).length === 0 || filter === "") {
+      return dataArr;
+    }
+    this.resource.data = [];
+    dataArr.forEach((row) => {
+      for (var value in row) {
+        if (row.hasOwnProperty(value)) {
+          let element;
+          if (typeof row[value] === "object") {
+            element = JSON.stringify(row[value]);
+          }
+          if (typeof row[value] === "boolean") {
+            element = "" + row[value];
+          }
+          if (typeof row[value] === "string") {
+            element = row[value].toLocaleLowerCase();
+          }
+          if (typeof row[value] === "number") {
+            element = "" + row[value];
+          }
+          if (element.indexOf(filter["value"].toLocaleLowerCase()) >= 0) {
+            this.resource.data.push(row);
             return;
+          }
         }
-        if (typeof filter === 'undefined' || Object.keys(filter).length === 0 || filter === "") {
-            return dataArr;
-        }
-        this.resource.data = [];
-        dataArr.forEach((row) => {
-            for (var value in row) {
-                if (row.hasOwnProperty(value)) {
-                    let element;
-                    if (typeof row[value] === "object") {
-                        element = JSON.stringify(row[value]);
-                    }
-                    if (typeof row[value] === "boolean") {
-                        element = "" + row[value];
-                    }
-                    if (typeof row[value] === "string") {
-                        element = row[value].toLocaleLowerCase();
-                    }
-                    if (typeof row[value] === "number") {
-                        element = "" + row[value];
-                    }
-                    if (element.indexOf(filter["value"].toLocaleLowerCase()) >= 0) {
-                        this.resource.data.push(row);
-                        return;
-                    }
-                }
-            }
-        });
+      }
+    });
 
-        return this.resource.data;
-    }
+    return this.resource.data;
+  }
 }
--- app/pipes/header-pipe.ts
+++ app/pipes/header-pipe.ts
@@ -3,48 +3,48 @@ import {FiltersService} from "../services/filters-service";
 import {ResourceService} from "../services/resource-service";
 
 @Pipe({
-    name: 'search'
+  name: 'search'
 })
 
 export class SearchPipe {
-    constructor(public filtersService:FiltersService,
-                public resource:ResourceService) {
+  constructor(public filtersService:FiltersService,
+              public resource:ResourceService) {
+  }
+
+  transform(value, [filters]) {
+    if (typeof value === "undefined") {
+      return;
+    }
+    this.resource.data = value.slice();
+
+    if (typeof filters === 'undefined' || Object.keys(filters).length === 0) {
+      return this.resource.data;
     }
 
-    transform(value, [filters]) {
-        if (typeof value === "undefined") {
+    this.filtersService.update(filters.key, filters.value);
+    let filtersArr = this.filtersService.get();
+
+    value.forEach((item) => {
+      for (var filterKey in filtersArr) {
+        if (filtersArr.hasOwnProperty(filterKey)) {
+          let element;
+          if (typeof item[filterKey] === "string") {
+            element = item[filterKey].toLocaleLowerCase();
+          }
+          if (typeof item[filterKey] === "object") {
+            element = JSON.stringify(item[filterKey]);
+          }
+          if (typeof item[filterKey] === "number") {
+            element = item[filterKey].toString();
+          }
+          if (element.indexOf(filtersArr[filterKey].toLocaleLowerCase()) === -1) {
+            this.resource.data.splice(this.resource.data.indexOf(item), 1);
             return;
+          }
         }
-        this.resource.data = value.slice();
+      }
+    });
 
-        if (typeof filters === 'undefined' || Object.keys(filters).length === 0) {
-            return this.resource.data;
-        }
-
-        this.filtersService.update(filters.key, filters.value);
-        let filtersArr = this.filtersService.get();
-
-        value.forEach((item) => {
-            for (var filterKey in filtersArr) {
-                if (filtersArr.hasOwnProperty(filterKey)) {
-                    let element;
-                    if (typeof item[filterKey] === "string") {
-                        element = item[filterKey].toLocaleLowerCase();
-                    }
-                    if (typeof item[filterKey] === "object") {
-                        element = JSON.stringify(item[filterKey]);
-                    }
-                    if (typeof item[filterKey] === "number") {
-                        element = item[filterKey].toString();
-                    }
-                    if (element.indexOf(filtersArr[filterKey].toLocaleLowerCase()) === -1) {
-                        this.resource.data.splice(this.resource.data.indexOf(item), 1);
-                        return;
-                    }
-                }
-            }
-        });
-
-        return this.resource.data;
-    }
+    return this.resource.data;
+  }
 }
