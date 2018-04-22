7277e523c0bb6afba02e0b77adcfd6e405bb6f9e
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2018-04-22 12:44:53 +0200

    reverted CsvExportComponent

---
+++ src/app/ngx-easy-table/components/csv-export.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-csv-export',
+  template: `
+    <a (click)="exportCsv()">
+      CSV export
+    </a>`
+})
+
+export class CsvExportComponent {
+  @Input() data;
+  public exportCsv() {
+    const data = this.data;
+    let csvContent = 'data:text/csv;charset=utf-8,';
+    let dataString = '';
+    const x: Array<any> = [];
+    const keys = Object.keys(this.data[0]);
+    data.forEach((row, index) => {
+      x[index] = [];
+      keys.forEach((i) => {
+        if (row.hasOwnProperty(i)) {
+          if (typeof row[i] === 'object') {
+            row[i] = 'Object'; // so far just change object to "Object" string
+          }
+          x[index].push(row[i]);
+        }
+      });
+    });
+
+    csvContent += keys + '\n';
+    x.forEach((row, index) => {
+      dataString = row.join(',');
+      csvContent += index < data.length ? dataString + '\n' : dataString;
+    });
+    const encodedUri = encodeURI(csvContent);
+    const link = document.createElement('a');
+    link.setAttribute('href', encodedUri);
+    link.setAttribute('download', 'my_data.csv');
+
+    link.click();
+  }
+}
