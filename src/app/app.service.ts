f8a1a9345e11a435d85265dbb83908a2aaf0736b
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2018-05-19 11:45:25 +0200

    since conditionally applied directive is not possible, table now is split into 2 different table - with drag and without

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -7,7 +7,7 @@ export class ConfigService {
     searchEnabled: false,
     headerEnabled: true,
     orderEnabled: true,
-    globalSearchEnabled: true,
+    globalSearchEnabled: false,
     paginationEnabled: true,
     exportEnabled: false,
     clickEvent: true,
@@ -24,6 +24,6 @@ export class ConfigService {
     collapseAllRows: false,
     checkboxes: false,
     resizeColumn: true,
-    draggable: false
+    draggable: true
   };
 }
