5e6f633d1eabea23bbede16916b7554443a8e057
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2018-05-22 16:04:52 +0200

    fix for AOT, lambda doesnt work

--- src/app/ngx-easy-table/services/filters.service.ts
+++ src/app/ngx-easy-table/services/filters.service.ts
@@ -2,5 +2,9 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class FiltersService {
-  static getPath = (p, o) => p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : null, o);
+  static getPath(p, o) {
+    // https://github.com/dherges/ng-packagr/issues/696
+    const result = p.reduce((xs, x) => (xs && xs[x]) ? xs[x] : null, o);
+    return result;
+  }
 }
