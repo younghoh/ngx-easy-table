4e809de6fbc7c6d281b53f85e473ac03b248d396
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2018-10-08 13:29:35 +0200

    first page improvements

--- src/app/welcome/welcome.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-basic',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css'],
-})
-export class WelcomeComponent {
-}
