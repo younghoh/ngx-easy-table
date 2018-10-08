4447d4e18f6fb5c47f3ed96b3521b8345ea84cd7
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2018-10-08 16:08:27 +0200

    fixed bug, where locale names were not sorted correctly

--- cypress/integration/sort.spec.ts
+++ cypress/integration/sort.spec.ts
@@ -16,4 +16,15 @@ context('Sort', () => {
       .get('#table > tbody > tr:nth-child(1) > td:nth-child(3) > div')
       .contains('ARCHITAX');
   });
+  it('gets correct name sorted by locale when "Name" clicked', () => {
+    cy
+      .get('#table > thead > tr > th:nth-child(4)').click()
+      .get('#table > thead > tr > th:nth-child(4)').click()
+      .get('#pagination-controls > pagination-template > ul > li:nth-child(5)').click()
+      .get('#table > tbody > tr:nth-child(1) > td:nth-child(4) > div').contains('Monica Frazier')
+      .get('#table > tbody > tr:nth-child(2) > td:nth-child(4) > div').contains('Mónica Glover')
+      .get('#table > tbody > tr:nth-child(3) > td:nth-child(4) > div').contains('Moody Blevins')
+      .get('#table > tbody > tr:nth-child(4) > td:nth-child(4) > div').contains('Myles Blair')
+    ;
+  });
 });
