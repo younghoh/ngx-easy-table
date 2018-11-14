d1c9c8adada1ade7d81382c9556c655c2874d2d3
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2018-11-14 06:42:35 +0100

    fixed export enums

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
 export * from './src/app/ngx-easy-table/table.module';
-export * from './src/app/ngx-easy-table/model';
+export * from './src/app/ngx-easy-table/model/config';
+export * from './src/app/ngx-easy-table/model/columns';
+export * from './src/app/ngx-easy-table/model/event.enum';
