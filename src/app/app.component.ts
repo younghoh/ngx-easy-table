8e974eebcd8c549656afe77a331b7f7ba7e5abc4
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2018-11-27 10:41:15 +0100

    mobile friendly menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { environment } from '../environments/environment';
 @Component({
   selector: 'app-table',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   public version: string = environment.VERSION;
