8bd90a82068c2764a62d8ff11f3074028caa71e2
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2019-02-27 16:45:40 +0100

    fixedColumnWidth is now enabled by default

--- src/app/demo/dynamic-row/configuration.service.ts
+++ src/app/demo/dynamic-row/configuration.service.ts
@@ -23,7 +23,7 @@ export class ConfigService {
     collapseAllRows: false,
     checkboxes: false,
     resizeColumn: false,
-    fixedColumnWidth: false,
+    fixedColumnWidth: true,
     horizontalScroll: false,
     draggable: false,
     logger: false,
