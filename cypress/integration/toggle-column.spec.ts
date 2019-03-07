925e8141c0116de635343c8df225f98261ccda9f
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2019-03-07 08:36:25 +0100

    search DOM need to be removed from the DOM instead setting style visibility

--- cypress/integration/toggle-column.spec.ts
+++ cypress/integration/toggle-column.spec.ts
@@ -11,7 +11,7 @@ context('Toggle column config', () => {
       .get('#table > tbody > tr:nth-child(1) > td:nth-child(1) > div').contains('+1 (949) 527-2108')
       .get('#table > tbody > tr:nth-child(1) > td:nth-child(2) > div').contains('36')
       .get('#table > tbody > tr:nth-child(1) > td:nth-child(3) > div').contains('KONGENE')
-      .get('#content > div > app-toggle-column > div:nth-child(4) > div > div > label:nth-child(2)').click()
+      .get('#content > div > app-toggle-column > div:nth-child(3) > div > div > label:nth-child(2)').click()
       .get('#table > tbody > tr:nth-child(1) > td:nth-child(1) > div').contains('+1 (949) 527-2108')
       .get('#table > tbody > tr:nth-child(1) > td:nth-child(2) > div').contains('KONGENE')
     ;
