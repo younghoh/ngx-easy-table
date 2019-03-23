aff9969a03104d511484cbb0e0514446710f6095
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2019-03-23 11:07:40 +0100

    default config has not been exported

--- projects/ngx-easy-table/src/public_api.ts
+++ projects/ngx-easy-table/src/public_api.ts
@@ -1,9 +1,7 @@
-/*
- * Public API Surface of lib
- */
 export * from './lib/table.module';
 export * from './lib/model/config';
 export * from './lib/model/columns';
 export * from './lib/model/event.enum';
 export * from './lib/model/pagination';
 export * from './lib/model/api';
+export * from './lib/services/config-service';
