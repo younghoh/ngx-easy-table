2fd0a05ae1afc7b81729cd336757f860185903e4
Sebastian Superczynski <sebastian.superczynski@espeo.eu>
2019-04-28 19:38:21 +0200

    additional tests

--- cypress/integration/pin.spec.ts
+++ cypress/integration/pin.spec.ts
@@ -11,6 +11,21 @@ context('Pinned column', () => {
       .get('#table > tbody > tr:nth-child(1) > td:nth-child(2)').should('have.class', 'pinned-left')
     ;
   });
+  it('first and second column is pinned when using pagination', () => {
+    cy
+      .get('#pagination-controls > ul > li:nth-child(4) > a')
+      .click()
+      .get('#table > tbody > tr:nth-child(1) > td:nth-child(1)').should('have.class', 'pinned-left')
+      .get('#table > tbody > tr:nth-child(1) > td:nth-child(2)').should('have.class', 'pinned-left')
+    ;
+  });
+  it('first and second column is pinned when search enabled', () => {
+    cy
+      .getInput('company').type('COLAIRE')
+      .get('#table > tbody > tr:nth-child(1) > td:nth-child(1)').should('have.class', 'pinned-left')
+      .get('#table > tbody > tr:nth-child(1) > td:nth-child(2)').should('have.class', 'pinned-left')
+    ;
+  });
   // TODO implement pin API
   // it('uses API to set 2nd column pinned', () => {
   //   cy
